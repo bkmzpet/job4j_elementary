52890cd16fa464ba3de30816b183108fe4fd3508
bkmzpet <bkmzpett@gmail.com>
2021-03-05 15:52:18 +0500

    6.0.2. Заполнение массива. [#471229]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,16 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[100500];
+        names[0] = "Коля";
+        names[1] = "Вася";
+        names[2] = "Петя";
+        names[3] = "Валя";
+        names[4] = "Дима";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+        System.out.println(names[4]);
     }
 }
