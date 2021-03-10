44c90779ba3f2dc0ae8dd1ba9d4b1cd5ca8f9514
github.com <Bkmzpett@gmsil.com>
2021-03-10 18:49:27 +0300

    6.9. Жадный алгоритм. Сдача в кофе машине [#471216]

--- src/ru/job4j/array/Defragment.java
+++ src/ru/job4j/array/Defragment.java
@@ -1,31 +1,23 @@
 package ru.job4j.array;
 
-    public class Defragment {
-        public static String[] compress(String[] array) {
-            for (int index = 0; index < array.length; index++) {
-                if (array[index] == null) {
-                    int point = index;
-                    for (int j = point; j < array.length; j++) {
-                        if (array[j] != null) {
-                            String temp = array[point];
-                            array[point] = array[j];
-                            array[j] = temp;
-                            break;
-                        }
-                    }
-
-                }
-
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                int point = index; /* указатель на null ячейку. */
+                /* переместить первую не null ячейку. Нужен цикл. */
             }
-            return array;
+            System.out.print(array[index] + " ");
         }
+        return array;
+    }
 
-        public static void main(String[] args) {
-            String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
-            String[] compressed = compress(input);
-            System.out.println();
-            for (int index = 0; index < compressed.length; index++) {
-                System.out.print(compressed[index] + " ");
-            }
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
         }
     }
+}
---
+++ src/ru/job4j/sort/Machine.java
@@ -0,0 +1,31 @@
+package ru.job4j.sort;
+
+import java.util.Arrays;
+
+public class Machine {
+    public static int[] change(int money, int price) {
+        int[] coins = {10, 5, 2, 1};
+        int[] rsl = new int[90];
+        int size = 0;
+        size = money - price;
+        for (int i = 0; i < coins.length; i++) {
+            while (coins[i] <= size) {
+                size -= coins[i];
+                rsl[i] = coins[i];
+                break;
+            }
+
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+}
+
+
+
+
+
+
+
+
+
+
